96d2c85db5c7f9bfc20003c263855e6ff27225c1
wilifabrizio <wili.fabrizio@gmail.com>
2014-08-27 21:10:26 -0300

    Primeros archivos

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Sistemas de becas UTN</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<section id="contenedor">
+		<header></header>
+		<section id="contenido"></section>
+		<footer></footer>
+	</section>
+</body>
+</html>
